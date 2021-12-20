35cf14ef86b23f9c14d3649c7f11a543148a2371
Saurabh Shetty <63945352+KeenBean024@users.noreply.github.com>
2021-12-21 00:34:26 +0530

    Time: 44 ms (85.39%), Space: 14.4 MB (48.76%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashMap = dict()
+        for index in range(len(nums)):
+            num=nums[index]
+            delta = target-num
+            if delta in hashMap:
+                return [index,hashMap[delta]]
+            else:
+                hashMap[num]=index
