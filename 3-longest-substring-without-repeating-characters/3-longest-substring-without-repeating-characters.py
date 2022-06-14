57e9691c595c3aa004826cbb959b9ff31ced1fcf
Saurabh Shetty <63945352+KeenBean024@users.noreply.github.com>
2022-06-14 13:15:58 +0530

    Time: 84 ms (47.50%), Space: 14.4 MB (21.29%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,18 +5,12 @@ class Solution(object):
         :rtype: int
         """
         l=0
-        r=0
         mem = set()
         max_len = 0
-        while r<len(s):
-            if s[r] not in mem:
-                mem.add(s[r])
-                r+=1
-            elif s[r] in mem:
-                max_len = max(max_len, r-l)
-                while s[l]!=s[r]:
-                    mem.remove(s[l])
-                    l+=1
+        for r in range(len(s)):
+            while s[r] in mem:
+                mem.remove(s[l])
                 l+=1
-                r+=1
-        return max(max_len, r-l)
+            mem.add(s[r])
+            max_len = max(max_len, r-l+1)
+        return  max_len
