58f4a145ee0499719fbb825c01b3898f4a53e9d9
Saurabh Shetty <63945352+KeenBean024@users.noreply.github.com>
2022-06-27 17:29:11 +0530

    Time: 30 ms (26.39%), Space: 13.3 MB (85.25%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,19 +1,36 @@
 class Solution(object):
+#     #Memoization
+#     def climbStairs(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         mem = {}
+#         def traverse(n):
+#             if n==0:
+#                 return 1
+#             if n<0:
+#                 return 0
+#             if n in mem:
+#                 return mem[n]
+#             else:
+#                 mem[n] = traverse(n-1) + traverse(n-2)
+#             return mem[n]
+        
+#         return traverse(n)
+    
+    #Dynamic Programming
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        mem = {}
-        def traverse(n):
-            if n==0:
-                return 1
-            if n<0:
-                return 0
-            if n in mem:
-                return mem[n]
-            else:
-                mem[n] = traverse(n-1) + traverse(n-2)
-            return mem[n]
-        
-        return traverse(n)
+        one, two = 1,1
+        for _ in range(n-1):
+            tmp = one
+            one = one+two
+            two=tmp
+        return one
+#Without Memoization - T O(2^N) S O(2^N)
+#With Memoization - T O(N) S O(N)
+#With DP T O(N) S O(1)
