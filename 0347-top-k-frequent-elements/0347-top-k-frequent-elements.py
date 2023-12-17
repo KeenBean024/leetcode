7e9d1afa523865b0115f2e30ee9868f497c17b1b
Saurabh Shetty <63945352+KeenBean024@users.noreply.github.com>
2023-12-17 17:43:21 -0500

    Time: 112 ms (16.21%), Space: 21 MB (5.15%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = defaultdict(lambda :0)
+        freq = [[] for _ in range(len(nums)+1)]
+        for n in nums:
+            count[n] += 1
+            
+        for key, value in count.items():
+            freq[value].append(key)
+       
+        print(freq)
+        result = []
+        index = len(freq)-1
+        while k>0:
+            if freq[index]:
+                result.extend(freq[index])
+                k-=len(freq[index])
+            index -= 1
+        return result
+            
